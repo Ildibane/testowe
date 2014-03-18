bc9b4696781b44d4fcca2d7605dfacfd5a4fab0b
Żychowicz Dariusz <dariusz.zychowicz@vsoft.pl>
2014-03-18 15:45:49 +0100

    komentarz1

---
+++ Test1/Controllers/AccountXController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test1.Models;
+using Test1;
+using System.Web.Security;
+
+namespace Test1.Controllers
+{
+    public class AccountXController : Controller
+    {
+        //
+        // GET: /AccountX/
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET: /Account/LogOn
+
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (new KitsulaMembershipProviter().ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (!String.IsNullOrEmpty(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "AccountX");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
